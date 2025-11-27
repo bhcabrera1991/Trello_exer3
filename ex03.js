cda0ac4df8d6487b1f181058700eb9f4899329af
bhcabrera1991 <brunohenriquecabrera@gmail.com>
2025-11-27 08:38:27 -0300

    Continuando

--- ex03.js
+++ ex03.js
@@ -1,10 +1,11 @@
 function fnMedia(){
-    let janeiro=0, feveiro=0, marco=0, media= 0
-    janeiro= document.getElementById("janeiro_2018").value
-    feveiro = document.getElementById("fevereiro_2018").value
-    marco = document.getElementById("marco_2018").value
-    media= janeiro+feveiro+marco
+    let janeiro =0,  fevereiro=0 , marco=0, media=0 
+    janeiro = Number( document.getElementById("janeiro_2018").value)
+    fevereiro = Number ( document.getElementById("fevereiro_2018").value)
+    marco = Number ( document.getElementById("marco_2018").value)
+    media= (janeiro+fevereiro+marco)/3
     document.getElementById("resultadoMedia").innerHTML=`Média de ${media}`
+    document.getElementById("janeiro_2018").value
     
 
 }
